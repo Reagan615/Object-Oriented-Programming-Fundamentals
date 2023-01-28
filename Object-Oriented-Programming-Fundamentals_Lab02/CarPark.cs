071c907e6b126a3e72a3eb1feeeb0ba359049643
Reagan615 <419247451@qq.com>
2023-01-27 21:32:32 -0600

    create

---
+++ Object-Oriented-Programming-Fundamentals_Lab02/CarPark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming_Fundamentals_Lab02
+{
+    internal class CarPark
+    {
+    }
+}
