44f70f5f645f60798fd2e5482f1f9d9954c014af
Reagan615 <419247451@qq.com>
2023-01-27 21:36:28 -0600

    commit

--- Object-Oriented-Programming-Fundamentals_Lab02/Vehicle.cs
+++ Object-Oriented-Programming-Fundamentals_Lab02/Vehicle.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Object_Oriented_Programming_Fundamentals_Lab02
 {
-    internal class Vehicle
+    public class Vehicle
     {
+        public HashSet<ParkingSpot> ParkingSpots = new HashSet<ParkingSpot>();
+        private string _license;
+        public string License { get { return _license; } }
+        public void UpdateLicenseNumber(string license)
+        {
+            if (license.Length < 3 || license.Length > 7 || !license.All(c => char.IsLetter(c)))
+            {
+                throw new Exception("License number must be between 3 and 7 alphanumeric characters long");
+            }
+            else
+            {
+                _license = license;
+            }
+
+        }
+
+        public Vehicle(string license)
+        {
+            UpdateLicenseNumber(license);
+
+        }
     }
 }
